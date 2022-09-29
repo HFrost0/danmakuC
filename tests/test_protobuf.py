f839394a7d75679876e99ac2b19b8c52ac5679a5
HFrost0 <2498393441@qq.com>
2022-09-29 17:03:10 +0800

    test update

--- tests/test_protobuf.py
+++ tests/test_protobuf.py
@@ -2,23 +2,33 @@ import time
 from danmakuC.old import proto2ass as proto2ass_old
 from danmakuC.utils import proto2ass
 
+file = 'test_dm_large.bin'
 
-def test_dm2ass():
+
+def test_cmp():
     """"
     test_dm.bin and test_dm_large.bin is obtained by bilix from bilibili.com
     """
-    a = time.time()
-    with open("test_dm_large.bin", "rb") as f:
-        text = proto2ass(f.read(), 1920, 1080)
-    print(time.time() - a)
-
-    a = time.time()
-    with open("test_dm.bin", "rb") as f:
-        text2 = proto2ass_old(f.read(), 1920, 1080)
-    print(time.time() - a)
-
+    text = test_new()
+    text2 = test_old()
     for l1, l2 in zip(text.split('\n'), text2.split('\n')):
         # todo different time convert due to ceil ?
         if l1 != l2:
             print(l1)
             print(l2)
+
+
+def test_new():
+    a = time.time()
+    with open(file, "rb") as f:
+        text = proto2ass(f.read(), 1920, 1080)
+    print(time.time() - a)
+    return text
+
+
+def test_old():
+    a = time.time()
+    with open(file, "rb") as f:
+        text = proto2ass_old(f.read(), 1920, 1080)
+    print(time.time() - a)
+    return text
