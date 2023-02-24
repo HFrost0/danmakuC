9d73314b073a662b147db2102bdfa4661a9f565a
HFrost0 <2498393441@qq.com>
2023-02-24 20:25:50 +0800

    simplify main and maintain same -o reaction with doc

--- danmakuC/__init__.py
+++ danmakuC/__init__.py
@@ -3,5 +3,4 @@ Faster conversion for larger Danmaku to Ass format
 https://github.com/HFrost0/danmakuC
 """
 
-from . import bilibili
-from . import niconico
+from . import bilibili, niconico
--- danmakuC/__main__.py
+++ danmakuC/__main__.py
@@ -1,42 +1,26 @@
 import argparse
 import gzip
-import io
 import os
-import sys
 from typing import Callable, Union
 
 from danmakuC.__version__ import __version__
 # sites
-from danmakuC import bilibili
-from danmakuC import niconico
+from danmakuC import bilibili, niconico
 
 
 def is_gzip_file(filename: Union[str, os.PathLike]) -> bool:
     gzip_magic = b'\x1f\x8b'
     with open(filename, mode='rb') as fp:
-        if fp.read(2) == gzip_magic:
-            res = True
-        else:
-            res = False
-    return res
+        return fp.read(2) == gzip_magic
 
 
-def get_convert_func(proto_file: Union[str, bytes, io.IOBase, os.PathLike], ) -> Callable:
-    if isinstance(proto_file, (str, os.PathLike)):
-        if is_gzip_file(proto_file):
-            open_func = gzip.open
-        else:
-            open_func = open
-        with open_func(proto_file, mode='rb') as fp:
-            firstbyte = fp.read(1)
-    elif isinstance(proto_file, bytes):
-        firstbyte = proto_file[0:1]
-    elif isinstance(proto_file, io.IOBase):
-        firstbyte = proto_file.read(1)
-        proto_file.seek(-1, 1)
+def get_convert_func(proto_file: Union[str, os.PathLike], ) -> Callable:
+    if is_gzip_file(proto_file):
+        open_func = gzip.open
     else:
-        raise TypeError('proto_file must be str, bytes, os.PathLike object or a file object')
-
+        open_func = open
+    with open_func(proto_file, mode='rb') as fp:
+        firstbyte = fp.read(1)
     if firstbyte == b'\x0a':
         convert_func = bilibili.proto2ass
     else:
@@ -91,37 +75,24 @@ def main():
     width, height = args.size.split("x")
     width = int(width)
     height = int(height)
-    fo = None
-    if args.output == '-':
-        fo = sys.stdout
-        out_filename = ''
-    elif args.output:
-        # fo = open(args.output, "w", encoding="utf-8-sig", errors="replace", newline="\r\n")
-        out_filename = args.output
-    else:
-        out_filename = args.file
-        if out_filename.endswith('.bin'):
-            out_filename = args.file[:-4]
-        elif out_filename.endswith('.bin.gz'):
-            out_filename = args.file[:-7]
-        out_filename += '.ass'
-    output = get_convert_func(args.file)(
-        args.file,
-        width,
-        height,
-        args.reserve_blank,
-        args.font,
-        args.fontsize,
-        args.alpha,
-        args.duration_marquee,
-        args.duration_still,
-        args.filter,
-        args.reduce,
-        out_filename=out_filename,
-    )
-    if fo:
-        fo.write(output)
-        fo.close()
+
+    with open(args.file, 'rb') as f:
+        res = get_convert_func(args.file)(
+            f,
+            width,
+            height,
+            args.reserve_blank,
+            args.font,
+            args.fontsize,
+            args.alpha,
+            args.duration_marquee,
+            args.duration_still,
+            args.filter,
+            args.reduce,
+            out_filename=args.output,
+        )
+    if res is not None:
+        print(res)
 
 
 if __name__ == "__main__":
