8e354c14b4fcdff047c13c0e2457069202f2a943
HFrost0 <2498393441@qq.com>
2023-04-13 13:29:33 +0800

    ci update

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
     classifiers=[
         'Programming Language :: C++',
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
