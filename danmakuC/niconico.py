adf636b0041c45538c47d44ef6af436f8c2ec28b
NEtUkA <69663281+NEtUkA@users.noreply.github.com>
2024-12-03 23:22:11 +0800

    Nico color mapping update  and structure refactor

--- danmakuC/niconico.py
+++ danmakuC/niconico.py
@@ -32,7 +32,8 @@ def proto2ass(
             break
         comment_serialized = proto_file.read(size)
         comment.ParseFromString(comment_serialized)
-        pos, color, size = process_mailstyle(comment.mail, font_size)
+        style, commands = process_mailstyle(comment.mail)
+        pos, color, size = style["pos"], style["color"], style["size"] * font_size
         ass.add_comment(
             comment.vpos / 100,
             comment.date,
@@ -43,41 +44,42 @@ def proto2ass(
         )
     if out_filename:
         return ass.write_to_file(out_filename)
-    else:
-        return ass.to_string()
+    return ass.to_string()
 
 
-def process_mailstyle(mail, fontsize):
-    pos, color, size, patissier = 0, 0xffffff, fontsize, False
-    if not mail:
-        return pos, color, size  # , patissier
-    for mailstyle in mail.split():
-        if mailstyle == 'ue':  # top middle
-            pos = 1
-        elif mailstyle == 'shita':  # bottom middle
-            pos = 2
-        elif mailstyle == 'naka':  # flying left-to-right
-            pos = 0
-        elif mailstyle == 'big':
-            size = fontsize * 1.44
-        elif mailstyle == 'small':
-            size = fontsize * 0.64
+def process_mailstyle(mail):
+    style = {"pos": 0, "size": 1, "color": 0xFFFFFF, "font": "defont"}
+    commands = {k: False for k in OTHERS}
+    if isinstance(mail, str):
+        mail = mail.split()
+    # only the first command of each type is used, see https://qa.nicovideo.jp/faq/show/6167
+    for mailstyle in (m.lower() for m in reversed(mail)):
+        if mailstyle in POS_MAPPING:
+            style["pos"] = POS_MAPPING[mailstyle]
+        elif mailstyle in SIZE_MAPPING:
+            style["size"] = SIZE_MAPPING[mailstyle]
         elif mailstyle in NICONICO_COLOR_MAPPINGS:
-            color = NICONICO_COLOR_MAPPINGS[mailstyle]
-        elif len(mailstyle) == 7 and re.match(HEX_COLOR_REGEX, mailstyle):
-            color = int(re.match(HEX_COLOR_REGEX, mailstyle).group(1), base=16)
-        elif mailstyle == 'patissier':  # for comment art/fixed speed?
-            patissier = True
+            style["color"] = NICONICO_COLOR_MAPPINGS[mailstyle]
+        elif match := HEX_COLOR_REGEX.match(mailstyle):
+            style["color"] = int(match.group(1), 16)
+        elif mailstyle in FONT_MAPPING:
+            style["font"] = FONT_MAPPING[mailstyle]
+        elif mailstyle in OTHERS:
+            commands[mailstyle] = True
+        elif match := DURATION_REGEX.match(mailstyle):
+            style["duration"] = float(match.group(2) or match.group(4))
+    style["alpha"] = 0.5 if commands.get("_live") else 1
+    return style, commands
 
-    return pos, color, size  # , patissier
 
-
+# https://dic.nicovideo.jp/a/コマンド
 # https://w.atwiki.jp/nicoapi/pages/20.html
 NICONICO_COLOR_MAPPINGS = {
     # Regular users
+    'white': 0xffffff,
     'red': 0xff0000,
     'pink': 0xff8080,
-    'orange': 0xffcc00,
+    'orange': 0xffc000,
     'yellow': 0xffff00,
     'green': 0x00ff00,
     'cyan': 0x00ffff,
@@ -95,10 +97,54 @@ NICONICO_COLOR_MAPPINGS = {
     'yellow2': 0x999900,
     'elementalgreen': 0x00cc66,
     'green2': 0x00cc66,
-    'marineblue': 0x33ffcc,
-    'blue2': 0x33ffcc,
+    'marineblue': 0x3399ff,
+    'blue2': 0x3399ff,
     'nobleviolet': 0x6633cc,
     'purple2': 0x6633cc,
+    'pink2': 0xff33cc,
+    'cyan2': 0x00cccc,
+    'black2': 0x666666,
 }
 
-HEX_COLOR_REGEX = re.compile('#([a-fA-F0-9]{6})')
+POS_MAPPING = {
+    "naka": 0, # flying left-to-right
+    "ue": 1, # top middle
+    "shita": 2, # bottom middle
+    # "＠逆": 3, # flying right-to-left, not a command
+
+    # http://himado.in/help.php?name=comment_command
+    "middle": 0,
+    "top": 1,
+    "bottom": 2,
+    # "hidari",
+    # "left",
+    # "chu",
+    # "center",
+    # "migi",
+    # "right",
+    # "(X,Y)",
+}
+
+SIZE_MAPPING = {
+    "small": 2/3,
+    "medium": 1,
+    "big": 13/9,
+}
+
+FONT_MAPPING = {
+    "mincho": "Yu Mincho", # Simsun
+    "gothic": "Yu Gothic", # Gulim, SimHei
+}
+
+OTHERS = {
+    # "184",
+    # "ca",
+    "invisible",
+    # "patissier",
+    "full",
+    "ender",
+    "_live", # alpha = 0.5
+}
+
+DURATION_REGEX = re.compile(r'^(@(\d+(\.\d+)?)|(\d+)sec)$')
+HEX_COLOR_REGEX = re.compile('^#([a-fA-F0-9]{6})$')
